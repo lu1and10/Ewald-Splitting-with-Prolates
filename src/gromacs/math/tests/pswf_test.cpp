4f8dedd7c191143a512bb85f4df3499ce95652da
Libin Lu <llu@flatironinstitute.org>
2026-05-22 05:09:40 -0400

    math: add pswf_test target with construction-range smoke test
    First test only validates the ctor argument range. Subsequent steps add:
    - Step 1.5: EvalAtZeroEqualsExpectedNormalization, EvalSymmetric, RefdataParity
    - Step 1.6: EvalDerivativeVsFiniteDifference
    - ...

---
+++ src/gromacs/math/tests/pswf_test.cpp
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the GROMACS molecular simulation package.
+ *
+ * Copyright 2026- The GROMACS Authors
+ * and the project initiators Erik Lindahl, Berk Hess and David van der Spoel.
+ * Consult the AUTHORS/COPYING files and https://www.gromacs.org for details.
+ *
+ * GROMACS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * GROMACS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GROMACS; if not, see
+ * https://www.gnu.org/licenses, or write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * If you want to redistribute modifications to GROMACS, please
+ * consider that scientific software is very special. Version
+ * control is crucial - bugs must be traceable. We will be happy to
+ * consider code for inclusion in the official distribution, but
+ * derived work must not be called official GROMACS. Details are found
+ * in the README & COPYING files - if they are missing, get the
+ * official version at https://www.gromacs.org.
+ *
+ * To help us fund GROMACS development, we humbly ask that you cite
+ * the research papers on the package. Check out https://www.gromacs.org.
+ */
+
+#include "gmxpre.h"
+
+#include "gromacs/math/pswf.h"
+
+#include <stdexcept>
+
+#include <gtest/gtest.h>
+
+namespace gmx::esp::test
+{
+namespace
+{
+
+TEST(Pswf0, ConstructionRangeChecks)
+{
+    EXPECT_THROW(Pswf0(0.0), std::invalid_argument);
+    EXPECT_THROW(Pswf0(-1.0), std::invalid_argument);
+    EXPECT_THROW(Pswf0(30.01), std::invalid_argument);
+    EXPECT_NO_THROW(Pswf0(5.0));
+    EXPECT_NO_THROW(Pswf0(12.024));
+}
+
+} // namespace
+} // namespace gmx::esp::test
